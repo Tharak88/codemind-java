582e416a96ccfe995411b0eb153f88b2b79cd4e8
tharak <96295224+Tharak88@users.noreply.github.com>
2022-06-22 09:49:38 +0530

    import java.util.*; class Sam {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch;         ch=sc.next().charAt(0);         int n=ch;         System.out.print(n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sam
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        ch=sc.next().charAt(0);
+        int n=ch;
+        System.out.print(n);
+    }
+}
