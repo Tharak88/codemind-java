1c42a835d8cfa7980a92b02a587f68a7ce87825e
tharak <96295224+Tharak88@users.noreply.github.com>
2022-06-22 11:11:53 +0530

    import java.util.*; class sam {     public static void main(String args[])     {       float a,b;         double c;         Scanner sc =new Scanner(System.in);                 a=sc.nextInt();         b=sc.nextInt();
            c=(a+b)/2;
            System.out.format("%.4f",c);
            
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sam
+{
+    public static void main(String args[])
+    {
+      float a,b;
+        double c;
+        Scanner sc =new Scanner(System.in);
+                a=sc.nextInt();
+        b=sc.nextInt();
+
+        c=(a+b)/2;
+        System.out.format("%.4f",c);
+        
+    }
+}
